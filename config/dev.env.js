67a26d4c7b6f12a8b4cff7558e5b8bed8d9e93f4
luzg <342166808@qq.com>
2019-12-14 17:30:17 +0800

    提交初始代码

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://www.fastmock.site/mock/b4a8e34133ea0e029607f77a4733b80a/ams"'
+})
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"https://www.easy-mock.com/mock/5df04c7db9fe1220269a0d0d"'
+}
